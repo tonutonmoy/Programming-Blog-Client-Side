ad494f272829c42c97f409eb045797ec4ec3e748
tonutonmoy <www.tonutonmoy12@gmail.com>
2024-06-17 14:46:51 +0600

    manage user done

--- src/Component/Dashboard/Admin/ManageUsers/ManageUsers.tsx
+++ src/Component/Dashboard/Admin/ManageUsers/ManageUsers.tsx
@@ -1,5 +1,145 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { Toaster, toast } from "sonner";
+const AllUsersGQL = gql`
+  query Users {
+    users {
+      name
+      role
+      email
+      id
+      profile {
+        image
+      }
+    }
+  }
+`;
+const UpdateUserRoleGQL = gql`
+  mutation UpdateUserRole($userId: ID!, $userRole: String!) {
+    updateUserRole(userId: $userId, userRole: $userRole) {
+      userError
+      result {
+        id
+      }
+    }
+  }
+`;
+
 const ManageUsers = () => {
-  return <div></div>;
+  const { loading, error, data, refetch } = useQuery(AllUsersGQL);
+  const [updateUserRole] = useMutation(UpdateUserRoleGQL);
+
+  if (loading) {
+    return null;
+  }
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+  console.log(data?.users);
+
+  const roleHandler = async (userId: string, userRole: string) => {
+    console.log(userId, userRole);
+    const updateData = await updateUserRole({
+      variables: { userId, userRole },
+    });
+
+    console.log(updateData, "updatedata");
+
+    if (updateData?.data?.updateUserRole?.result?.id) {
+      toast.success("Role updated successfully");
+      refetch();
+    }
+    if (updateData?.data?.updateUserRole?.userError) {
+      toast.error(updateData?.data?.updateUserRole?.userError);
+    }
+  };
+
+  return (
+    <section className="relative py-16 bg-blueGray-50">
+      <div className="w-full mb-12 px-4">
+        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-gray-700 text-white">
+          <div className="rounded-t mb-0 px-4 py-3 border-0">
+            <div className="flex flex-wrap items-center">
+              <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                <h3 className="font-semibold text-lg text-center text-white"></h3>
+              </div>
+            </div>
+          </div>
+          <div className="block w-full overflow-x-auto">
+            <table className="items-center w-full bg-transparent border-collapse">
+              <thead>
+                <tr>
+                  <th className="px-6 align-center text-center border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold  bg-gray-800  border-pink-700">
+                    Name
+                  </th>
+                  <th className="px-6 text-center align-middle border border-solid py-3 text-xs uppercase  border-r-0 whitespace-nowrap font-semibold  bg-gray-800  border-pink-700 border-l-pink-700">
+                    Email
+                  </th>
+                  <th className="px-6 text-center align-middle border border-solid py-3 text-xs uppercase border-r-0 whitespace-nowrap font-semibold  bg-gray-800  border-pink-700 border-l-pink-700">
+                    Role
+                  </th>
+                  <th className="px-6 text-center align-middle border border-solid py-3 text-xs uppercase border-r-0 whitespace-nowrap font-semibold  bg-gray-800  border-pink-700 border-l-pink-700">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data?.users?.map((project: any) => (
+                  <tr key={project?.id}>
+                    <th className="border m-4  rounded-lg px-6 align-middle  text-xs whitespace-nowrap p-4 text-left flex items-center ">
+                      <img
+                        src={project?.profile?.image}
+                        className="h-12 w-12 bg-white rounded-full border"
+                        alt="..."
+                      />
+                      <span className="ml-3 font-bold text-white">
+                        {project?.name}
+                      </span>
+                    </th>
+                    <td>
+                      <p className="px-6 align-middle   text-sm whitespace-nowrap p-4  border m-4  rounded-lg ">
+                        {project?.email}
+                      </p>
+                    </td>
+                    <td>
+                      <p className="px-6 align-middle  text-center  text-sm whitespace-nowrap p-4  border m-4  rounded-lg">
+                        {project?.role}
+                      </p>
+                    </td>
+                    <td>
+                      {project?.role === "admin" && (
+                        <p className="px-6 align-middle flex justify-center    text-xs whitespace-nowrap p-4  border m-4  rounded-lg">
+                          <button
+                            onClick={() => roleHandler(project?.id, "user")}
+                            className="w-[130px] flex justify-center bg-blue-400 hover:bg-blue-500 text-gray-100 p-3 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500"
+                          >
+                            Make user
+                          </button>
+                        </p>
+                      )}
+                      {project?.role === "user" && (
+                        <p className="px-6 align-middle  flex justify-center  text-xs whitespace-nowrap p-4  border m-4  rounded-lg">
+                          <button
+                            onClick={() => roleHandler(project?.id, "admin")}
+                            className="w-[130px] flex justify-center bg-green-400 hover:bg-green-500 text-gray-100 p-3 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500"
+                          >
+                            Make Admin
+                          </button>
+                        </p>
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <Toaster position="top-right" />
+    </section>
+  );
 };
 
 export default ManageUsers;
--- src/SharedComponent/Sidebar/SideBarLinks.tsx
+++ src/SharedComponent/Sidebar/SideBarLinks.tsx
@@ -1,7 +1,29 @@
 import { Link } from "react-router-dom";
 import LoginAndLogout from "../LoginAndLogout/LoginAndLogout";
+import { gql, useQuery } from "@apollo/client";
+
+const SingleGQL = gql`
+  query SingleUser {
+    singleUser {
+      role
+    }
+  }
+`;
 
 const SidebarAllLinks = () => {
+  const { loading, error, data } = useQuery(SingleGQL);
+
+  if (loading) {
+    return null;
+  }
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+
+  console.log(data?.singleUser?.role);
+
   return (
     <div>
       <ul className="menu p-4   w-52 md:w-80 lg:w-80  xl:w-80 2xl:w-80   min-h-full bg-base-200 text-base-content">
@@ -12,52 +34,52 @@ const SidebarAllLinks = () => {
                 Travel-Buddy
               </Link>
             </>
-            {/* {data?.data?.role === "User" && ( */}
-            <>
-              <Link
-                to="/dashboard/myBlogs"
-                className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
-              >
-                My Blogs
-              </Link>
+            {data?.singleUser?.role === "user" && (
+              <>
+                <Link
+                  to="/dashboard/myBlogs"
+                  className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
+                >
+                  My Blogs
+                </Link>
 
-              <Link
-                to="/dashboard/profile"
-                className="flex items-center px-3 py-2.5 font-bold hover:text-indigo-900 hover:border rounded-full"
-              >
-                Profile
-              </Link>
-            </>
-            {/* )} */}
+                <Link
+                  to="/dashboard/profile"
+                  className="flex items-center px-3 py-2.5 font-bold hover:text-indigo-900 hover:border rounded-full"
+                >
+                  Profile
+                </Link>
+              </>
+            )}
 
-            {/* {data?.data?.role === "Admin" && ( */}
-            <>
-              <Link
-                to="/dashboard/manageUsers"
-                className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
-              >
-                Manage Users
-              </Link>
-              <Link
-                to="/dashboard/publishedBlogs"
-                className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
-              >
-                Published Blogs
-              </Link>
-              <Link
-                to="/dashboard/RequestedBlogs"
-                className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
-              >
-                Requested Blogs
-              </Link>
-              <Link
-                to="/dashboard/profile"
-                className="flex items-center px-3 py-2.5 font-bold hover:text-indigo-900 hover:border rounded-full"
-              >
-                Profile
-              </Link>
-            </>
-            {/* )} */}
+            {data?.singleUser?.role === "admin" && (
+              <>
+                <Link
+                  to="/dashboard/manageUsers"
+                  className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
+                >
+                  Manage Users
+                </Link>
+                <Link
+                  to="/dashboard/publishedBlogs"
+                  className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
+                >
+                  Published Blogs
+                </Link>
+                <Link
+                  to="/dashboard/RequestedBlogs"
+                  className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
+                >
+                  Requested Blogs
+                </Link>
+                <Link
+                  to="/dashboard/profile"
+                  className="flex items-center px-3 py-2.5 font-bold hover:text-indigo-900 hover:border rounded-full"
+                >
+                  Profile
+                </Link>
+              </>
+            )}
           </div>
           <div className="  flex items-center px-10  pt-40 font-semibold hover:text-indigo-900 hover:border hover:rounded-full">
             <LoginAndLogout />
