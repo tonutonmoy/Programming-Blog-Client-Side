a6fb800f95767fd08e746ff7b397bca365bd49c9
tonutonmoy <www.tonutonmoy12@gmail.com>
2024-06-20 20:04:10 +0600

    add active link

--- src/SharedComponent/Sidebar/SideBarLinks.tsx
+++ src/SharedComponent/Sidebar/SideBarLinks.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import LoginAndLogout from "../LoginAndLogout/LoginAndLogout";
 import { gql, useQuery } from "@apollo/client";
 
@@ -12,6 +12,9 @@ const SingleGQL = gql`
 
 const SidebarAllLinks = () => {
   const { loading, error, data } = useQuery(SingleGQL);
+  const location = useLocation();
+
+  console.log(location?.pathname, "location");
 
   if (loading) {
     return null;
@@ -26,7 +29,7 @@ const SidebarAllLinks = () => {
 
   return (
     <div>
-      <ul className="menu p-4   w-52 md:w-80 lg:w-80  xl:w-80 2xl:w-80   min-h-full h-screen  bg-gradient-to-r from-gray-500 to-gray-400  text-gray-50">
+      <ul className="menu p-4   w-52 md:w-80 lg:w-80  xl:w-80 2xl:w-80   min-h-full h-screen  bg-green-400 text-gray-50">
         <aside className="py-4 w-full md:block">
           <div className="sticky flex flex-col gap-2 p-4 text-sm border-r border-gray-100 top-12 rounded-md">
             <>
@@ -38,14 +41,20 @@ const SidebarAllLinks = () => {
               <>
                 <Link
                   to="/dashboard/myBlogs"
-                  className="flex items-center px-3 py-2.5 font-semibold hover:text-gray-100 hover:bg-gray-700 hover:border hover:rounded-full"
+                  className={`flex items-center px-3 py-2.5 font-semibold ${
+                    location?.pathname === "/dashboard/myBlogs" &&
+                    "text-gray-100 bg-green-500 border rounded-full"
+                  }  `}
                 >
                   My Blogs
                 </Link>
 
                 <Link
                   to="/dashboard/profile"
-                  className="flex items-center px-3 py-2.5 font-bold hover:text-gray-100 hover:bg-gray-700 hover:border rounded-full"
+                  className={`flex items-center px-3 py-2.5 font-semibold ${
+                    location?.pathname === "/dashboard/profile" &&
+                    "text-gray-100 bg-green-500 border rounded-full"
+                  }  `}
                 >
                   Profile
                 </Link>
@@ -56,25 +65,37 @@ const SidebarAllLinks = () => {
               <>
                 <Link
                   to="/dashboard/manageUsers"
-                  className="flex items-center px-3 py-2.5 font-semibold hover:text-gray-100 hover:bg-gray-700 hover:border hover:rounded-full"
+                  className={`flex items-center px-3 py-2.5 font-semibold ${
+                    location?.pathname === "/dashboard/manageUsers" &&
+                    "text-gray-100 bg-green-500 border rounded-full"
+                  }  `}
                 >
                   Manage Users
                 </Link>
                 <Link
                   to="/dashboard/publishedBlogs"
-                  className="flex items-center px-3 py-2.5 font-semibold hover:text-gray-100 hover:bg-gray-700 hover:border hover:rounded-full"
+                  className={`flex items-center px-3 py-2.5 font-semibold ${
+                    location?.pathname === "/dashboard/publishedBlogs" &&
+                    "text-gray-100 bg-green-500 border rounded-full"
+                  }  `}
                 >
                   Published Blogs
                 </Link>
                 <Link
                   to="/dashboard/RequestedBlogs"
-                  className="flex items-center px-3 py-2.5 font-semibold hover:text-gray-100 hover:bg-gray-700 hover:border hover:rounded-full"
+                  className={`flex items-center px-3 py-2.5 font-semibold ${
+                    location?.pathname === "/dashboard/RequestedBlogs" &&
+                    "text-gray-100 bg-green-500 border rounded-full"
+                  }  `}
                 >
                   Requested Blogs
                 </Link>
                 <Link
                   to="/dashboard/profile"
-                  className="flex items-center px-3 py-2.5 font-bold hover:text-gray-100   hover:bg-gray-700 hover:border rounded-full"
+                  className={`flex items-center px-3 py-2.5 font-semibold ${
+                    location?.pathname === "/dashboard/profile" &&
+                    "text-gray-100 bg-green-500 border rounded-full"
+                  }  `}
                 >
                   Profile
                 </Link>
